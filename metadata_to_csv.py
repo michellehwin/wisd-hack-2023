560145aea6278eff152b4f2f5a6556b45b432837
Michelle Nguyen <michellehn@utexas.edu>
2023-07-12 19:00:45 -0400

    add notebook

--- metadata_to_csv.py
+++ metadata_to_csv.py
@@ -1,6 +1,8 @@
 import json
 import pandas as pd
 
+"""Generate a CSV file with games metadata and team"""
+
 games_file = open('metadata/games.json')
 games_json = json.load(games_file)
 data=[]
@@ -9,7 +11,6 @@ for game in games_json['games']:
     data.append(game)
 
 games_df=pd.DataFrame(data)
-  
 games_file.close()
 
 teams_file = open('metadata/teams.json')
@@ -19,7 +20,7 @@ for game in teams_json['teams']:
     data.append(game)
 
 teams_df=pd.DataFrame(data)
-  
 teams_file.close()
 
 games_df.merge(teams_df, left_on='homeTeamId', right_on='id')
+games_df.to_csv('games_w_teams.csv')
