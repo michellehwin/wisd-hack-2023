3ca1f4fbfe23c1ba447bb3b9bb28df1aa7ac6644
Michelle Nguyen <michellehn@utexas.edu>
2023-07-30 02:42:26 -0400

    simple rest api using flask

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import pandas as pd
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/coords")
+def hello_world():
+    game_id = request.args.get('game_id')
+
+    # play_rank can be 1-10, 1 being best play
+    play_rank = request.args.get('play_rank')
+
+    if (game_id is None and play_rank is None):
+        return "Please supply game_id and play_rank"
+    
+    coord_file = open(f"./play_coords/{game_id}_coords_by_player.json")
+    coord_json = json.load(coord_file)
+    return coord_json[str(play_rank)]
